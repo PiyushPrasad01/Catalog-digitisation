48feceace7f2491fe9c13393b0cefbf9d529e465
Piyush Kumar Prasad <128150786+PiyushPrasad01@users.noreply.github.com>
2024-03-06 09:16:27 +0530

    Add files via upload

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,1 +1,38 @@
+import dotenv from 'dotenv';
 
+import mongoose from "mongoose";
+// import { DB_NAME } from "../constants.js";// 
+const DB_NAME="ONDCdata";
+	
+dotenv.config({
+    path: '../env'
+})
+
+// console.log(DB_NAME);
+
+
+async function connectDB(){
+
+    // console.log("Connecting: ",process.env.MONGODB_URI)
+
+    try {
+		const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        if(connectionInstance)
+        {
+            console.log(`\n connected to database`)
+        }
+
+		
+	} catch (error) {
+		console.log("Error(while connecting to db): ", error);
+
+        process.exit(1);
+	}   
+
+}
+
+
+
+
+export default connectDB;
