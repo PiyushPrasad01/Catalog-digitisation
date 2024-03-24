b7067aa47aedce92da9779a9b91178ecab7e2a5d
Piyush Kumar Prasad <128150786+PiyushPrasad01@users.noreply.github.com>
2024-03-24 12:19:29 +0530

    Update category.js

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,1 +1,20 @@
+import { Router } from "express";
+import Category  from "../models/category.model.js";
 
+const router=new Router()
+
+router.post("/",async(req,res)=>{
+    
+    try {
+        const successObject = await Category.create({
+            categories:req.body.category
+        });
+        res.status(200).json({message:`${req.body.category} category has been created`,success:true})
+        // console.log("successObject: ",successObject);
+    } catch (error) {
+        res.status(503).json({message:error.message,success:false})
+        console.log(error)
+    }
+})
+
+export default router;
