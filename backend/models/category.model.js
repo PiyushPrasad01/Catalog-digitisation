4f22e41975c6f778819c25801850340217b46a6f
Piyush Kumar Prasad <128150786+PiyushPrasad01@users.noreply.github.com>
2024-03-24 12:16:08 +0530

    Update category.model.js

--- backend/models/category.model.js
+++ backend/models/category.model.js
@@ -1,1 +1,21 @@
+import mongoose from 'mongoose';
 
+const {model , Schema} = mongoose;
+
+const categorySchema = new Schema({
+    
+    'categories':{
+        type:'String',
+        unique:true,
+    }
+
+},{timestamps: true}
+)
+
+// const Category =model("category",categorySchema)
+// module.exports=Category;
+
+const Category = model(
+    'Category',categorySchema
+)
+export default Category;
