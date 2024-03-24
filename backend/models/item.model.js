ee9b42e5acfb682ab8fa1c66d36605590ae17e3f
Piyush Kumar Prasad <128150786+PiyushPrasad01@users.noreply.github.com>
2024-03-24 12:18:05 +0530

    Update item.model.js

--- backend/models/item.model.js
+++ backend/models/item.model.js
@@ -1,1 +1,46 @@
+import mongoose from 'mongoose';
 
+const {model , Schema} = mongoose;
+
+const itemsSchema = new Schema({
+    
+    name:{
+        type:String,
+        unique:true,
+        required:true,
+    },
+    category:{
+        type: String,
+        required:true,
+        ref:'Category',
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    img_url:{
+        type:[String],
+        required:true,
+    },
+    price:{
+        type:Number,
+        default:0,
+        required:[true,"This can't be empty"],
+    },
+    stock:{
+        type:Number,
+        default:0,
+        required:true,
+    }
+
+},{timestamps: true}
+)
+
+
+// const Items =model("blog",itemsSchema)
+// module.exports=Items;
+const Items = model(
+    'Item',itemsSchema
+)
+
+export default Items;
